f907ac48bd94f2bfb4db28f65e424b9d408ce70f
Henrique Santos <47578636+henriquesantoss@users.noreply.github.com>
2022-05-25 17:47:19 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,34 @@ onScroll()
 function onScroll() {
     showNavOnScroll()
     showBackToTopButtonOnScroll()
+    ativarMenuNaSecaoMomento(home)
+    ativarMenuNaSecaoMomento(services)
+    ativarMenuNaSecaoMomento(about)
+    ativarMenuNaSecaoMomento(contact)
 }
+
+function ativarMenuNaSecaoMomento(section) {
+    const TargetLine = scrollY + innerHeight /2
+
+    const secaoTop = section.offsetTop
+    const secaoAltura = section.offsetHeight
+    const chegouOuPassouNaLinha = TargetLine >= secaoTop
+
+    const ondeTermina = secaoTop + secaoAltura
+    const finalSecaoPassouNaLinha = ondeTermina <= TargetLine
+
+    const limiteSecao = chegouOuPassouNaLinha && !finalSecaoPassouNaLinha
+
+    const sectionId = section.getAttribute('id')
+    const menuElemento = document.querySelector(`.menu 
+    a[href*=${sectionId}]`)
+    
+    menuElemento.classList.remove('active')
+    if (limiteSecao) {
+        menuElemento.classList.add('active')
+    }
+}
+
 
 function showNavOnScroll() {
     if(scrollY > 0) {
